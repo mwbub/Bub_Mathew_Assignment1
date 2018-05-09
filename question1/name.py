d9a5fd28fd0abaae8c3a9c0d02756119f567ca82
Mathew Bub <mathew.bub@mail.utoronto.ca>
2018-05-09 16:24:32 -0400

    Added initial test code for question 1

--- question1/name.py
+++ question1/name.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python
+
+import glob
+
+find = '.'
+replace = ','
+
+for filename in glob.glob('*.txt'):
+	f = open(filename, 'r')
+	string = f.read()
+	f.close()
+	replaced = string.replace(find, replace)
+	print(replaced)
